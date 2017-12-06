38c4a7a8217b35c70082698cef2031de3728b4a4
KayBeard <kbeard@apterainc.com>
2017-12-06 15:38:20 -0500

    feat: created visit login method

---
+++ AllTheWayAutomatedPractice/AllTheWayAutomatedTests/PageObjects/HomePage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+
+namespace AllTheWayAutomatedTests.PageObjects
+{
+    class HomePage : BasePage
+    {
+        public HomePage(IWebDriver driver) : base(driver) { }
+
+        public void VisitCustomerLogin()
+        {
+            Visit("");
+            By CustomerLoginButton = SelectorByAttributeValue("ng-click", "customer()");
+            WaitUntilDisplayed(CustomerLoginButton, WAIT_SECONDS);
+            Click(CustomerLoginButton);
+        }
+    }
+}
