e4cfa2d497b94711920c039fc8c04f6c9e6e0172
KayBeard <kbeard@apterainc.com>
2017-12-06 14:34:50 -0500

    Initial Project Setup

---
+++ AllTheWayAutomatedPractice/AllTheWayAutomatedTests/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllTheWayAutomatedTests
+{
+    [TestFixture]
+    public class AllTheWayAutomatedTestsContext
+    {
+       public static TestContext Context { get; private set; }
+
+        public static void AssemblyInit(TestContext context)
+        {
+            Context = context;
+        }
+    }
+}
